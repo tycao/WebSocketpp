b013f6921f45e817753cf44c1bcba3bb89802b2a
caosx <616881845@qq.com>
2018-11-18 23:52:18 +0800

    添加sql.sql

---
+++ version0.1/sql.sql
@@ -0,0 +1,7 @@
+create table if not exists `m_user_info` (
+	login_info varchar(1000)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+create table if not exists `m_user_info_his` (
+	login_info varchar(1000)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
